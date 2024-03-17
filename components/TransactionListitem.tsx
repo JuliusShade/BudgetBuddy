44ba783b3f0fc69b03a1289b0ae76d03257e358c
JuliusShade <shadejr.business@gmail.com>
2024-03-16 21:41:02 -0400

    Added styling via the TransactionListItem and added constants to root

--- components/TransactionListitem.tsx
+++ components/TransactionListitem.tsx
@@ -1,5 +1,8 @@
+import { AntDesign } from "@expo/vector-icons";
 import { Category, Transaction } from "../types";
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { AutoSizeText, ResizeTextMode } from "react-native-auto-size-text";
+import { categoryColors, categoryEmojies } from "../constants";
 import Card from "./ui/card";
 
 interface TransactionListItemProps {
@@ -11,11 +14,62 @@ export default function TransactionListItem({
   transaction,
   categoryInfo,
 }: TransactionListItemProps) {
+  const iconName =
+    transaction.type === "Expense" ? "minuscircle" : "pluscircle";
+  const color = transaction.type === "Expense" ? "red" : "green";
+  const categoryColor = categoryColors[categoryInfo?.name ?? "Default"];
+  const emoji = categoryEmojies[categoryInfo?.name ?? "Default"];
   return (
     <Card>
+      <Amount amount={transaction.amount} color={color} iconName={iconName} />
       <Text>
         {categoryInfo?.name} amount: {transaction.amount}
       </Text>
     </Card>
   );
 }
+
+function Amount({
+  iconName,
+  color,
+  amount,
+}: {
+  iconName: "minuscircle" | "pluscircle";
+  color: string;
+  amount: number;
+}) {
+  return (
+    <View>
+      <AntDesign name={iconName} size={18} color={color} />
+      <AutoSizeText
+        fontSize={32}
+        mode={ResizeTextMode.max_lines}
+        numberOfLines={1}
+        style={[styles.amount, { maxWidth: "80%" }]}
+      >
+        ${amount}
+      </AutoSizeText>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  amount: {
+    fontSize: 32,
+    fontWeight: "800",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 6,
+  },
+  categoryContainer: {
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 3,
+    alignSelf: "flex-start",
+  },
+  categoryText: {
+    fontSize: 12,
+  },
+});
